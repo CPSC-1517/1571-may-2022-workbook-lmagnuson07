a885b9fa4372def63de758c833c36f60f5f0488d
logmagnuson <logmagns07@gmail.com>
2022-05-27 18:54:00 -0600

    Driver rotuine coded/composite class review.

--- ReviewSolution/OOPsReview/Program.cs
+++ ReviewSolution/OOPsReview/Program.cs
@@ -1,4 +1,6 @@
-﻿using OOPsReview.Data;
+﻿// using the "using statement" means that one does NOT need to fully qualify on EACH usage of the class
+using OOPsReview.Data;
+//this class is by default in the namespace of the project: OOPsReview 
 
 // fully qualified data member
 // OOPsReview.Data.SupervisoryLevel item = new SupervisoryLevel();
@@ -7,20 +9,96 @@
 // You cannot create your own instance of a static class
 // static classes are meant to be shared
 
-Console.WriteLine("Hello World!");
+
 
 // an instance class needs to be created using the new command and the class constructor
-// one needs to declare a variable of datatype Employment 
+// one needs to declare a variable of class datatype: ex Employment 
 
-Employment myEmp = new Employment(); // default constructor
-Employment myEmp1 = new Employment("Level 5 programmer", SupervisoryLevel.Supervisor, -15.9);
-Console.WriteLine(myEmp1.ToString()); // use the instance name to reference items within your class
+//Employment myEmp = new Employment(); // default constructor
 
-// invalid
-// Console.WriteLine(Employment.ToString());
+// fully qualified reference to Employment
+// consists of the namespace.classname
+// OOPsReview.Data.Employment myEmp1 = new Employment("Level 5 programmer", SupervisoryLevel.Supervisor, -15.9);
+Employment myEmp1 = new Employment("Level 5 programmer", SupervisoryLevel.Supervisor, 15.9);
+Console.WriteLine(myEmp1.ToString());
+Console.WriteLine($"{myEmp1.Title},{myEmp1.Level},{myEmp1.Years}");
+//Console.WriteLine(myEmp1.ToString()); // use the instance name to reference items within your class
 
-myEmp.SetEmplyeeResponsibilityLevel(SupervisoryLevel.DepartmentHead);
+//// invalid
+//// Console.WriteLine(Employment.ToString());
+
+myEmp1.SetEmplyeeResponsibilityLevel(SupervisoryLevel.DepartmentHead);
 
 Console.WriteLine(myEmp1.ToString());
 
-SupervisoryLevel item = new SupervisoryLevel();
+// SupervisoryLevel item = new SupervisoryLevel();
+
+// testing (simulate a Unit test) 
+// Arrange (setup of your test data)
+Employment Job = null;
+
+// passing a reference variable to a method 
+// a class is a reference data store 
+// this passes the actual memory address of the data store to the method 
+// ANY changes done to the data store within the method WILL BE reflected 
+// in the data store WHEN you return from the method
+
+CreateJob(ref Job);
+Console.WriteLine(Job.ToString());
+
+// passing value arguments to a method AND receiving a value result back from the method 
+// struct is a value data store 
+ResidentAddress Address = CreateAddress();
+Console.WriteLine(Address.ToString());
+
+// Act (execution of the test you wish to perform)
+// test that we can crate a person (Composite instance)
+Person me = null; // a variable capable of holding a Person instance
+me = CreatePerson(Job, Address);
+
+// Access (check your results)
+Console.WriteLine($"{me.FirstName} {me.LastName} lives at {me.Address.ToString()} having a job count of {me.NumberOfPositions}");
+
+void CreateJob(ref Employment job)
+{
+    // since the class MAY throw exceptions, you should have user friendly error handling
+    try
+    {
+        job = new Employment(); // default constructor; new command takes a constructor as it's reference
+        // because my properties have public set (mutators), I can "set" the value of the property directly from the driver program
+        job.Title = "Boss";
+        job.Level = SupervisoryLevel.Owner;
+        job.Years = 25.5;
+        // or 
+
+        // use the greedy constructor
+        // job = new Employment("Boss", SupervisoryLevel.Owner, 25.5);
+    }
+    catch (ArgumentNullException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    catch (ArgumentOutOfRangeException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    
+}
+
+ResidentAddress CreateAddress()
+{
+
+    ResidentAddress address = new ResidentAddress(10706, "106 st", "", "", "Edmonton", "AB");
+    return address;
+}
+
+Person CreatePerson(Employment job, ResidentAddress address)
+{
+    Person me = new Person("Don", "Welch", address, null);
+    me.AddEmployment(job);
+    return me;
+}
