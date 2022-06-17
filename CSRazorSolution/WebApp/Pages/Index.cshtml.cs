eb5055ca613a2bd5be5b453e013a5d509218f526
lmagnuson07 <logmagns07@gmail.com>
2022-06-17 17:29:19 -0600

    Create webapp ReadMe file

---
+++ CSRazorSolution/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string MyName { get; set; }
+        public void OnGet()
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(0, 100); // 100 is not included
+            if (value % 2 == 0)
+            {
+                MyName = $"Don ({value}) welcome to the wide wild world of Razor pages";
+            }
+            else
+            {
+                MyName = null;
+            }
+        }
+    }
+}
---
+++ CSRazorSolution/WebApp/ReadMe.md
@@ -0,0 +1,11 @@
+# 1517 May 2022 workbook
+
+## Logan Magnuson
+
+## lmagnuson07
+
+This is a public repository for my classroom demos on Razor Pages
+
+## Known Bugs
+
+no known bugs
