a1666f4e8ce849e9a7e2d2f4b43a676778c41f1c
lmagnuson07 <logmagns07@gmail.com>
2022-06-17 18:50:15 -0600

    Added a page to the environment.

--- CSRazorSolution/WebApp/Pages/Index.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Index.cshtml.cs
@@ -3,18 +3,34 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages
 {
+    // this web page Model class inherits from PageModel
     public class IndexModel : PageModel
     {
+        // This default page uses a system class called ILogger<T>
+        // This is composition
+        // This is a local field 
         private readonly ILogger<IndexModel> _logger;
 
+        // Constructor
+        // this constructor receives an injection of a service
+        // This injection is referred to as Injection Dependencies 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+        // This is a local property 
         public string MyName { get; set; }
+
+        // this is a class Behaviour (method)
+        // This method, OnGet(), executes for any Get request
+        // this method will be the first method executed when the page is first used 
+        // Excellent "event" to use to do any initialization to your web page display
+
         public void OnGet()
         {
+            // Once in the request method, you are in control of what is being processed on the web page for the current request 
+            // The code within this method is the work that I WISH to be done 
             Random rnd = new Random();
             int value = rnd.Next(0, 100); // 100 is not included
             if (value % 2 == 0)
@@ -25,6 +41,8 @@ namespace WebApp.Pages
             {
                 MyName = null;
             }
+
+            // control is returned to the web server 
         }
     }
 }
