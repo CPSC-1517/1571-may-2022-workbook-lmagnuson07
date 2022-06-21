56811efedcbef661dc5d3e64ab368e7dc89354f3
lmagnuson07 <logmagns07@gmail.com>
2022-06-20 18:53:55 -0600

    Added form elements and properties on the backend

--- CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -5,8 +5,29 @@ namespace WebApp.Pages.Samples
 {
     public class BasicDataManagementModel : PageModel
     {
+        // BindProperty is an annotation that handles two-way data transfer
+        // two-way? means output to form for display AND input from form for processing 
+        // Handles the output here 
+        [BindProperty]
+        public int Num { get; set; }
+        [BindProperty]
+        public string MassText { get; set; }
+        public string Feedback { get; set; }
         public void OnGet()
         {
+            if (Num == 0)
+            {
+                Feedback = "executing the OnGet method for the Get request";
+            }
+            else
+            {
+                Feedback = $"You entered the value {Num} displayed by OnGet";
+            }
+        }
+        public void OnPost()
+        {
+            Feedback = $"You entered the value {Num} displayed by OnPost\n" + 
+                $"your mass text is {MassText}";
         }
     }
 }
